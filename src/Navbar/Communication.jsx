2b4e2475385eb9821fa1b4f94c28871e347f3739
Kevin <arthurkevin1260@gmail.com>
2023-09-20 23:15:21 +0100

    added another component

--- src/Navbar/Communication.jsx
+++ src/Navbar/Communication.jsx
@@ -1,17 +1,19 @@
+import communicationPicture from '../assets/communication.png'
 const Communication = () => {
     return (
         <div>
           
-          <div id="header">
-<h1>
+          <div id="header" className='flex flex-col items-center'>
+<h1 className='font-bold text-[45px] text-[#060640]'>
     Global Promotion
 </h1>
-<h3>
+<h3 className='text-[16px] w-[764px] text-center text-[#9090A7]'>
 Businesses generally promote their brand, products, and services by identifying audience. No wonder that promotion strategy is one of the most important processes in marketing.
 
 </h3>
           </div>
-          
+
+<img src={communicationPicture} alt='a communication interface'/>          
            </div>
       );
 }
